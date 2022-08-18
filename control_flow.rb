4cb660b90a5cb1862679b9682b38d356ffde005a
simon waronja <simon.waronja@student.moringaschool.com>
2022-08-18 18:38:13 +0300

    done

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,65 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    puts "Access granted"
+    
+   else
+    puts "Access Denied"
+   end
 end
+
 
 def hows_the_weather(temperature)
-  # your code here
+
+ if temperature < 40
+    puts "brisk"
+
+ elsif (temperature >= 40 && temperature <= 65)
+    puts "a little chilly" 
+
+ elsif temperature > 85
+
+  puts "too dand hot"
+
+ else 
+  puts "perfect"
+ end
 end
 
+
 def fizzbuzz(num)
-  # your code here
+ if (num % 3 === 0 && num % 5 === 0)
+    puts "FizzBuzz"
+
+ elsif (num % 3 === 0)  
+  
+  puts "Fizz"
+  
+ elsif (num % 5 === 0)
+
+  puts "Buzz"
+
+ else 
+  puts num
+ end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
-end
+  
+  case "+"
+    when num1 + num2
 
+  case "-"
+    when num1 - num2
+    
+  case "*"
+    when num1 * num2
+    
+  case "/"
+    when num1 / num2
+
+    else 
+    puts "Invalid operation!"
+
+  end
+  
+end 
